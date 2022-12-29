0e337ff88a65387ed514477c2523298abad2b3e3
SeungheonShin <gody8756@naver.com>
2022-12-29 19:27:12 +0900

    chore: 제외 디렉토리 추가

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,14 @@
 const { merge } = require('webpack-merge');
 const config = require('./webpack.config');
+const path = require('path');
 
 module.exports = merge(config, {
   mode: 'development',
   devtool: 'inline-source-map',
+  watchOptions: {
+    ignored: [
+      path.posix.resolve(__dirname, 'node_modules'),
+      path.posix.resolve(__dirname, 'dist'),
+    ],
+  },
 });
