45e95f35803d35014e7fd884fc339ffadb3a24be
SeungheonShin <gody8756@naver.com>
2022-12-30 21:50:38 +0900

    feat: 무한스크롤 일부 적용

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,20 +8,30 @@ import { SimpleIconProps } from '@/interfaces/simpleIcon.interface';
 import * as Styles from '@/styles/App.style';
 
 const IconStyleContext = createContext('');
+const allIcons: SimpleIcon[] = Object.values(icons);
+const pageOffset = 16 as const;
 
 export default function App(): JSX.Element {
   const [keyword, setKeyword] = useState<string>('');
+  const [page, setPage] = useState<number>(0);
 
   const handleKeywordChange = (keyword: string): void => {
     setKeyword(keyword);
   };
 
-  const simpleIcons: SimpleIconProps[] = Object.values(icons)
-    .slice(0, 100)
+  console.log(
+    allIcons.filter(({ title }: SimpleIcon) =>
+      title.toLowerCase().includes('spring'),
+    ),
+  );
+
+  const simpleIcons: SimpleIconProps[] = allIcons
     .filter(({ title }: SimpleIcon) => title.toLowerCase().includes(keyword))
-    .map(({ title, hex, svg }: SimpleIcon) => {
+    .slice(page * pageOffset, page * pageOffset + pageOffset)
+    .map(({ title, slug, hex, svg }: SimpleIcon) => {
       return {
         title,
+        slug,
         hex,
         svg,
       };
