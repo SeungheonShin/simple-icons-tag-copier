8ec0c13afb4741fcfc66f7d5c72d165c4fd41a48
SeungheonShin <gody8756@naver.com>
2023-01-01 19:28:16 +0900

    feat: 로고 기본 색상 추가

--- src/styles/IconButton.style.tsx
+++ src/styles/IconButton.style.tsx
@@ -1,7 +1,6 @@
-import { convertSvgToDataUrl } from '@/utils/icon.util';
 import styled from 'styled-components';
 
-export const IconButton = styled.button`
+export const IconButton = styled.button<{ hex: string }>`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -13,13 +12,11 @@ export const IconButton = styled.button`
   background-color: rgba(0, 0, 0, 0);
   cursor: pointer;
   border: none;
-`;
 
-export const Icon = styled.img<{ svg: string }>`
-  content: url(${(props): string => convertSvgToDataUrl(props.svg)});
-
-  width: 48px;
-  height: 48px;
+  svg {
+    width: 48px;
+    height: 48px;
+  }
 `;
 
 export const Title = styled.p`
--- src/utils/icon.util.ts
+++ src/utils/icon.util.ts
@@ -1,11 +1,16 @@
-export const convertSvgToDataUrl = (svg: string): string =>
-  `data:image/svg+xml;utf8,${encodeURIComponent(svg)}`;
+export const getColoredSvg = (svg: string, hex: string): string => {
+  return svg.replace(/<path/, `<path fill="#${hex}"`);
+};
 
 export const getTagStr = ({
   title,
   hex,
+  logoColor,
 }: {
   title: string;
   hex: string;
+  logoColor: string;
 }): string =>
-  `<img src="https://img.shields.io/badge/${title}-${hex}?style=flat-square&logo=${title}&logoColor=white"/>`;
+  `<img src="https://img.shields.io/badge/${title}-${hex}?style=flat-square&logo=${title}&logoColor=${logoColor.slice(
+    1,
+  )}"/>`;
